59521d96f2415495fbc917dd0309cb8cd308a240
myth-21 <23eg112e12@anurag.edu.in>
2026-05-31 20:31:15 +0530

    updated files of backend files  again

--- Backend/server.js
+++ Backend/server.js
@@ -23,7 +23,12 @@ import { ensureSeedData } from './utils/ensureSeed.js';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json({ limit: '10mb' }));
 app.use('/uploads', express.static('public/uploads'));
 app.use(passport.initialize());
